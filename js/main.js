6a644e149f8c9cf58b69f33550f29ef32c16f007
cyrilf7 <cyrilf7x@gmail.com>
2012-12-16 23:08:42 -0500

    JS manage orientation (to do horizontal + img/div/html..

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@
  */
 
 (function($) {
-
 	$.fn.laserize = function(options) {
 
 		var opts = $.extend({}, $.fn.laserize.defaults, options);
@@ -17,14 +16,14 @@
 			var $this = $(this);
 			var $childrens = $this.children();
 
-			var bg = $childrens[0];
-			var hover = $childrens[1];
+			var bg = $($childrens[0]);
+			var hover = $($childrens[1]);
 
 			var thisW = $this.width();
 			var thisH = $this.height();
 
-			var imgBg = $(bg).attr('src');
-			var imgHover = $(hover).attr('src');
+			var imgBg = bg.attr('src');
+			var imgHover = hover.attr('src');
 
 			$this.html('<div class="item"><div class="hover"><div class="laser"></div></div></div>');
 
@@ -35,6 +34,14 @@
 			$('.hover').css('background','url('+imgHover+')');
 			$('.hover').css('background-size', thisW + 'px ' + thisH + 'px' );
 
+			if(opts.orientation === 'vertical') {
+				$('.hover').css({'width':'100%'});
+				$('.laser').css({'bottom':'-2px','width':'100%','height':'3px'});
+			} else {
+				$('.hover').css({'height':'100%'});
+				$('.laser').css({'right':'-2px','width':'3px','height':'100%'});
+			}
+
 			$('.laser').css('background', opts.laserColor);
 			$('.laser').css('box-shadow', '0 0 23px 7px '+ opts.laserColor);
 		}); // each
@@ -44,9 +51,9 @@
 		laserColor: 'red',
 		orientation: 'vertical'
 	};
-
 })(jQuery);
 
+//Call to the jquery function
 $(function() {
 	$('#element').laserize({laserColor:'#82f167'});
 });
