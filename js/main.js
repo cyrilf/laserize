1d89a75245f58f079009a636be60c85e9283a619
cyrilf7 <cyrilf7x@gmail.com>
2012-12-17 22:33:34 -0500

    Remove css beacause it's in the js now. Horizontal + Vertical supported

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 
 /*
- * Laserize a jQuery Plugin
- * Transform two images onto an hover laser effect
- *
- * Copyright 2012 CYRIL F, http://cyrilf.com
- * Released under the MIT License
- */
+* Laserize a jQuery Plugin
+* Transform two images onto an hover laser effect
+*
+* Copyright 2012 CYRIL F, http://cyrilf.com
+* Released under the MIT License
+*/
 
 (function($) {
 	$.fn.laserize = function(options) {
@@ -27,25 +27,36 @@
 
 			$this.html('<div class="item"><div class="hover"><div class="laser"></div></div></div>');
 
-			$('.item').css('height', thisH+'px');
-			$('.item').css('width', thisW+'px');
-			$('.item').css('background','url('+imgBg+')');
-			$('.item').css('background-size', thisW + 'px ' + thisH + 'px' );
-			$('.hover').css('background','url('+imgHover+')');
-			$('.hover').css('background-size', thisW + 'px ' + thisH + 'px' );
+			$this.find('.item').css({'height': thisH+'px', 'width': thisW+'px', 'background':'url('+imgBg+')', 'background-size': thisW + 'px ' + thisH + 'px'});
+			$this.find('.hover').css({'position':'relative','z-index':'2','background':'url('+imgHover+')','background-size': thisW + 'px ' + thisH + 'px' });
+			$this.find('.laser').css({'position':'absolute','border-radius':'50%','opacity':'0', 'background':opts.laserColor, 'box-shadow':'0 0 23px 7px '+ opts.laserColor});
 
+			var itemHover = {'height':'100%'};
+			var itemNonHover = {'height':'0'};
 			if(opts.orientation === 'vertical') {
-				$('.hover').css({'width':'100%'});
-				$('.laser').css({'bottom':'-2px','width':'100%','height':'3px'});
+				$this.find('.hover').css({'width':'100%', 'height':'0'});
+				$this.find('.laser').css({'bottom':'-2px','width':'100%','height':'3px'});
+				$this.find('.hover').css('transition', 'height .85s ease');
 			} else {
-				$('.hover').css({'height':'100%'});
-				$('.laser').css({'right':'-2px','width':'3px','height':'100%'});
+				$this.find('.hover').css({'height':'100%', 'width':'0'});
+				$this.find('.laser').css({'right':'-2px','width':'3px','height':'100%'});
+				$this.find('.hover').css('transition', 'width .85s ease');
+				itemHover = {'width':'100%'};
+				itemNonHover = {'width':'0'};
 			}
 
-			$('.laser').css('background', opts.laserColor);
-			$('.laser').css('box-shadow', '0 0 23px 7px '+ opts.laserColor);
-		}); // each
-	}; // opts
+			$this.find(".item").hover(
+					function () {
+						$this.find('.hover').css(itemHover);
+						$this.find('.laser').css({'opacity':'1', 'transition-delay':'0s'});
+					},
+					function () {
+						$this.find('.hover').css(itemNonHover);
+						$this.find('.laser').css({'opacity':'0', 'transition':'opacity .25s ease', 'transition-delay':'.75s'});
+					}
+				);
+	}); // each
+}; // opts
 
 	$.fn.laserize.defaults = {
 		laserColor: 'red',
@@ -55,5 +66,5 @@
 
 //Call to the jquery function
 $(function() {
-	$('#element').laserize({laserColor:'#82f167'});
+	$('.laserZone').laserize({laserColor:'#82f167', orientation:'vertical'});
 });
